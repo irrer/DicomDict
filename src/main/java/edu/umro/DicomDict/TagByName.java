bca21dc9af646695fe9b2464819d1ef0e16c3e51
Jim Irrer <irrer@umich.edu>
2021-07-28 08:35:04 -0400

    added license

--- src/main/java/edu/umro/DicomDict/TagByName.java
+++ src/main/java/edu/umro/DicomDict/TagByName.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2021 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.umro.DicomDict;
 
 import com.pixelmed.dicom.AttributeTag;
 import com.pixelmed.dicom.DicomDictionary;
 
 /**
- * Define a <code>AttributeTag</code> list that includes standard tags that are not included
+ * Define an <code>AttributeTag</code> list that includes standard tags that are not included
  * by the new Pixelmed DICOM dictionary, but are used by UMRO programs.
  * <p>
  * The name <code>TagFromName</code> allows simple migration by changing import statements as follows:
